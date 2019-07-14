f62613a27109ac1ebad237b8fa46e4506907bc0d
Mauricio Guerrero Tomas <mauricioguerrerotomas@gmail.com>
2019-07-14 02:32:29 -0500

    agregamos las funciones del services workers

---
+++ script.js
@@ -0,0 +1,6 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.egister('/sw.js')
+    .then(reg=> console.log('Registro del SW exitoso', reg)).
+    catch(err=>console.warn("Error al tratar de registrar el SW",err))
+
+}
