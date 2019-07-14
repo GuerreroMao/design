836d530551935f9565bc8905be6c8217664e8efb
Mauricio Guerrero Tomas <mauricioguerrerotomas@gmail.com>
2019-07-14 03:04:49 -0500

    realizamos el commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.egister('sw.js')
+    navigator.serviceWorker.register('sw.js')
     .then(reg=> console.log('Registro del SW exitoso', reg)).
     catch(err=>console.warn("Error al tratar de registrar el SW",err))
 
