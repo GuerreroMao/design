fb4bdfa0d4f5c3ea619a881a567024a6648b6ff8
Mauricio Guerrero Tomas <mauricioguerrerotomas@gmail.com>
2019-07-24 00:21:52 -0500

    activate sw

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,7 @@
 //install service worker
-self.addEventListener('install',(evt => {
+self.addEventListener('install',evt => {
     console.log('create service worker install');
-}));
+});
+self.addEventListener('activate',evt => {
+    console.log('service worker has been activated');
+})
