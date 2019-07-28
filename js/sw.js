015c091bb0d3e98677e6d0b5c64f30f2ca32d973
Mauricio Guerrero Tomas <mauricioguerrerotomas@gmail.com>
2019-07-28 01:32:51 -0500

    actualizamos el service worker

--- js/sw.js
+++ js/sw.js
@@ -10,31 +10,3 @@ self.addEventListener('activate',evt => {
 self.addEventListener('fetch',evt=>{
 
 })
-
-// App install banner
-// window.addEventListener('beforeinstallprompt', function(e) {
-//   e.userChoice.then(function(choiceResult){
-//       console.log(choiceResult.outcome);
-//       if(choiceResult.outcome == 'dismissed'){
-//           console.log('User cancelled home screen install');
-//       }else{
-//           console.log('User added to home screen');
-//       }
-//   });
-// });
-// btnAdd.addEventListener('click', (e) => {
-//   // hide our user interface that shows our A2HS button
-//   btnAdd.style.display = 'none';
-//   // Show the prompt
-//   deferredPrompt.prompt();
-//   // Wait for the user to respond to the prompt
-//   deferredPrompt.userChoice
-//     .then((choiceResult) => {
-//       if (choiceResult.outcome === 'accepted') {
-//         console.log('User accepted the A2HS prompt');
-//       } else {
-//         console.log('User dismissed the A2HS prompt');
-//       }
-//       deferredPrompt = null;
-//     });
-// });
