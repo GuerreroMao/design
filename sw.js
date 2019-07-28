897563f66d0b13874b1c86b2e5038f7b32dcf59d
Mauricio Guerrero Tomas <mauricioguerrerotomas@gmail.com>
2019-07-28 03:25:26 -0500

    remove

--- sw.js
+++ sw.js
@@ -12,14 +12,4 @@ self.addEventListener('fetch',evt=>{
     console.log('fetch event',evt);
 })
 
-var deferredPrompt;
 
-        window.addEventListener('beforeinstallprompt', function (e) {
-          // Prevent Chrome 67 and earlier from automatically showing the prompt
-          e.preventDefault();
-          // Stash the event so it can be triggered later.
-          deferredPrompt = e;
-
-          showAddToHomeScreen();
-          alert('hola mundo');
-        });
