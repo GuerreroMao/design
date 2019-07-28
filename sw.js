c0642a38e2477e2c7dd03db97f4a607a8509de58
Mauricio Guerrero Tomas <mauricioguerrerotomas@gmail.com>
2019-07-28 03:23:28 -0500

    agregamos el wndo

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ self.addEventListener('fetch',evt=>{
 
 var deferredPrompt;
 
-        self.addEventListener('beforeinstallprompt', function (e) {
+        window.addEventListener('beforeinstallprompt', function (e) {
           // Prevent Chrome 67 and earlier from automatically showing the prompt
           e.preventDefault();
           // Stash the event so it can be triggered later.
